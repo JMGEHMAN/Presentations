ebbceca39cb4df25c156abe68008443bff4e2611
Justin Gehman <32458329+jmgehman@users.noreply.github.com>
2019-09-02 23:31:06 -0700

    Building Write-Progress

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Presentations
 
-Date | Organization | Event | Title
----|---|---|---|---
-Sept 21, 2019 | Research Triangle PowerShell Users Group | PowerShell Saturday | PowerShell Streams and Using the Right "Write-*" Cmdlet
+| Date | Organization | Event | Title |
+| ---|---|---|---|--- |
+| Sept 21, 2019 | Research Triangle PowerShell Users Group | PowerShell Saturday | PowerShell Streams and Using the Right "Write-*" Cmdlet |
--- RTPSUG/PowerShell Saturday 2019/0 - Jumping Right In With Write-Progress.ps1
+++ RTPSUG/PowerShell Saturday 2019/0 - Jumping Right In With Write-Progress.ps1
@@ -1,11 +1,11 @@
-#Using Write-Progress to run a presentation
+#Write-Progress is great for providing status updates during long-running operations...like a presentation
 
 $max = 73
 
 for ($i = 1; $i -le $max; $i++) {
     $percentComplete = $i / $max * 100
 
-    Write-Progress -Activity 'Activity' -Status "[$i/$max ($percentComplete %) Status" -PercentComplete $percentComplete -CurrentOperation 'CurrentOperation'
+    Write-Progress -Activity 'Activity' -Status "$i/$max ($($percentComplete)%)" -PercentComplete $percentComplete -CurrentOperation 'CurrentOperation'
     Start-Sleep -Seconds 1
 }
 
@@ -36,7 +36,7 @@ for ($i = 1; $i -le $max; $i++) {
     Start-Sleep -Seconds 1
 }
 
-#Using 'Write-Progress' to run a presentation
+#<----- Using 'Write-Progress' to run a presentation ----->
 #   * All files named <ChapterNumber>_<TopicNumber>_<Parent ID(optional)>_<Chapter Title>_<Topic>.ps1
 
 $presentationFiles = Get-ChildItem -Path .\*_*.ps1 | Sort-Object
@@ -46,23 +46,39 @@ foreach ($file in $presentationFiles) {
     $splitFileName = $file.name -split '_'
     $i++
 
-    $progressParams = @{
-        id = $splitFileName[0 - 1]
+    ise $file.fullName
 
-    }
-
+    #File is a child
     if ($splitFileName[2] -as [int]) {
         $childFiles = Get-ChildItem -Path ".\*_*_$($splitFileName[2])_*.ps1" | Sort-Object
+        $childPercentComplete = [math]::Ceiling($childFiles.IndexOf(($childFiles | where name -eq $file.Name))+1 / $childFiles.Count * 100)
 
-        ParentId = $splitFileName[2]
-        PercentComplete = [math::Ceiling($childFiles.IndexOf($file) / $childFiles.Count * 100)
+        $progressParams = @{
+            id               = $splitFileName[0, 1] -join ''
+            ParentId         = $splitFileName[2]
+            Activity         = $splitFileName[3]
+            CurrentOperation = $splitFileName[4]
+            Status           = "$($childFiles.IndexOf(($childFiles | where name -eq $file.Name))+1)/$($childFiles.count) ($($childPercentComplete)%)"
+            PercentComplete  = $childPercentComplete
+        }
+
     } else {
-        PercentComplete = [math]::Ceiling($i / $presentationFiles.Count * 100)
+        $PercentComplete = [math]::Ceiling($i / $presentationFiles.Count * 100)
+        $progressParams = @{
+            id               = $splitFileName[0, 1] -join ''
+            Activity         = $splitFileName[2]
+            CurrentOperation = $splitFileName[3]
+            Status           = "$i/$($presentationFiles.count) ($($percentComplete)%)"
+            PercentComplete  = $percentComplete
+        }
 
     }
-
-
 
     Write-Progress @progressParams
 
+    do {
+        Start-Sleep -Seconds 2
+    } until ($psise.PowerShellTabs.Files.DisplayName -notcontains $file.Name)
+
 }
+
