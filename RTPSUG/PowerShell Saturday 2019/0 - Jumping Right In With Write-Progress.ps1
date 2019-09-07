e8e6b9e9f1e547f8cd181e4078f1fa69392a9c9a
Justin Gehman <32458329+jmgehman@users.noreply.github.com>
2019-09-07 10:40:36 -0700

    File names version 2

--- RTPSUG/PowerShell Saturday 2019/0 - Jumping Right In With Write-Progress.ps1
+++ RTPSUG/PowerShell Saturday 2019/0 - Jumping Right In With Write-Progress.ps1
@@ -1,15 +1,25 @@
 #Write-Progress is great for providing status updates during long-running operations...like a presentation
+#Looping through a list of actions and providing progress output
 
 $max = 73
 
+#
 for ($i = 1; $i -le $max; $i++) {
     $percentComplete = $i / $max * 100
 
-    Write-Progress -Activity 'Activity' -Status "$i/$max ($($percentComplete)%)" -PercentComplete $percentComplete -CurrentOperation 'CurrentOperation'
+    $progressParams = @{
+        Activity         = 'Activity Description Here'
+        Status           = "$i of $max ($($percentComplete)%)"
+        PercentComplete  = $percentComplete
+        CurrentOperation = 'CurrentOperation Description Here'
+        SecondsRemaining = $max - $i
+    }
+
+    Write-Progress @progressParams 
     Start-Sleep -Seconds 1
 }
 
-#<----- TIP: Making 'PercentComplete' and nice number ----->
+#<----- TIP: Making 'PercentComplete' a more readable number ----->
 #   * Using "Round", or "Ceiling" to make '$percentComplete' a nice number if you are going to diplay it
 #   * Completely optional, but I usually like to display current number, total number, and percentage
 
@@ -22,24 +32,32 @@ $max = 73
 #13/73*100 = 17.8082191780822, which is not a nice number
 $i / $max * 100
 
-#Using ROUND: Second number after the comma (,) sets the precision (number of decimal places)
+#Example using ROUND: Second number after the comma (,) sets the precision (number of decimal places)
 [math]::Round($i / $max * 100, 2)
 
-#Using CEILING: Result is rounded up to the next whole number
+#Example using CEILING: Result is rounded up to the next whole number
 [math]::Ceiling($i / $max * 100)
 
 #A nicely-formatted 'percentComplete' in action
 for ($i = 1; $i -le $max; $i++) {
-    $percentComplete = [math]::Round($i / $max * 100, 1)
+    $percentComplete = [math]::Round($i / $max * 100, 1)   ##<-- Rounding '$percentComplete to 1 decimal place
 
-    Write-Progress -Activity 'Activity' -Status "[$i/$max ($percentComplete %) Status" -PercentComplete $percentComplete -CurrentOperation 'CurrentOperation'
+    $progressParams = @{
+        Activity         = 'Activity Description Here'
+        Status           = "$i of $max ($($percentComplete)%)"
+        PercentComplete  = $percentComplete
+        CurrentOperation = 'CurrentOperation Description Here'
+        SecondsRemaining = $max - $i
+    }
+
+    Write-Progress @progressParams 
     Start-Sleep -Seconds 1
 }
 
 #<----- Using 'Write-Progress' to run a presentation ----->
-#   * All files named <ChapterNumber>_<TopicNumber>_<Parent ID(optional)>_<Chapter Title>_<Topic>.ps1
+#   * All files named "<Presentation Order>_<Progress ID>_<Parent ID(optional)>_<Activity/Chapter Title>_<CurrentOperation/Topic>.ps1"
 
-$presentationFiles = Get-ChildItem -Path .\*_*.ps1 | Sort-Object
+$presentationFiles = Get-ChildItem -Path .\*_*_*.ps1 | Sort-Object
 $i = 0
 
 foreach ($file in $presentationFiles) {
@@ -51,7 +69,7 @@ foreach ($file in $presentationFiles) {
     #File is a child
     if ($splitFileName[2] -as [int]) {
         $childFiles = Get-ChildItem -Path ".\*_*_$($splitFileName[2])_*.ps1" | Sort-Object
-        $childPercentComplete = [math]::Ceiling($childFiles.IndexOf(($childFiles | where name -eq $file.Name))+1 / $childFiles.Count * 100)
+        $childPercentComplete = [math]::Ceiling($childFiles.IndexOf(($childFiles | where name -eq $file.Name)) + 1 / $childFiles.Count * 100)
 
         $progressParams = @{
             id               = $splitFileName[0, 1] -join ''
@@ -81,4 +99,3 @@ foreach ($file in $presentationFiles) {
     } until ($psise.PowerShellTabs.Files.DisplayName -notcontains $file.Name)
 
 }
-
